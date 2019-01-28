33454258427a5e233619973aa711c4aad1804b83
Oleg <root@0x90.ru>
2019-01-28 06:51:31 +0300

    initial

---
+++ clone_all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+git clone --recursive git://github.com/0x90/wifi-arsenal
+git clone --recursive git://github.com/0x90/ism-arsenal
+git clone --recursive git://github.com/0x90/gsm-arsenal
+git clone --recursive git://github.com/0x90/bluetooth-arsenal
+git clone --recursive git://github.com/0x90/sdr-arsenal
+git clone --recursive git://github.com/0x90/atheros-arsenal
+git clone --recursive git://github.com/0x90/broadcom-arsenal
+git clone --recursive git://github.com/0x90/firmware-arsenal
+git clone --recursive git://github.com/0x90/esp-arsenal
+git clone --recursive git://github.com/0x90/vpn-arsenal
+git clone --recursive git://github.com/0x90/ss7-arsenal
+git clone --recursive git://github.com/0x90/nrf51-arsenal
+git clone --recursive git://github.com/0x90/nrf24-arsenal
